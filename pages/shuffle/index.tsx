b21f699a6416d82e955bd6fb333e94f7e4df0733
Huanchao Chen <huanchao.chen@vretta.com>
2022-06-27 18:05:27 -0400

    init shuffle game

--- pages/shuffle/index.tsx
+++ pages/shuffle/index.tsx
@@ -1,9 +1,54 @@
+import { useEffect } from "react";
 import useSuffleStore from "./store"
+import { uuid } from 'uuidv4'
 
 function ShuffleGame() {
-    const items = useSuffleStore((state: any) => state.items);
+    const items = useSuffleStore((state: any) => state.items)
+    const newItem = useSuffleStore((state: any) => state.newItem)
+    console.log(newItem);
     console.log(items);
-    return <div>About</div>
+    const initNewItem = useSuffleStore((state: any) => state.initNewItem)
+    const addItem = useSuffleStore((state: any) => state.addItem)
+    const editNewItem = useSuffleStore((state:any) => state.editNewItem)
+    const removeItem = useSuffleStore((state:any) => state.removeItem)
+
+    const addItemToList = () => {
+        addItem({
+            ...newItem,
+            id: uuid()
+        })
+        initNewItem()
+    }
+
+    useEffect(() => {
+        initNewItem()
+    }, [])
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">List your items</h1>
+
+            <div>
+                <div>
+                    <span>Content: </span>
+                    <input value={newItem?.content} onChange={(e) => editNewItem(e.target.value)}/>
+                    <button onClick={addItemToList}>Add</button>
+                </div>
+
+                {
+                    items.map((item: any, index: number) => {
+                        return (
+                            <div key={item.id}>
+                                {index + 1}
+                                {item.content}
+                                <button onClick={() => removeItem(item.id)}>remove</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
 }
   
 export default ShuffleGame
--- pages/shuffle/store.js
+++ pages/shuffle/store.js
@@ -1,8 +1,23 @@
 import create from 'zustand'
 
-const useSuffleStore = create(set => ({
-  items: [],
-  add: (item) => set(state => ({ items: [...state.items, item] })),
+
+const defaultItem = {
+    id: -1,
+    content: "",
+    hide: true,
+}
+
+const useSuffleStore = create((set) => ({
+    items: [],
+    newItem: defaultItem,
+    addItem: (item) => set(state => ({ items: [...state.items, item] })),
+    removeItem: (itemId) => set(state => {
+        state.items.splice(state.items.findIndex(item => item.id === itemId), 1)
+
+        return { items: [...state.items] }
+    }),
+    initNewItem: () => set(_ => ({ newItem: defaultItem })),
+    editNewItem: (content) => set(state => ({ newItem: { ...state.newItem, content }}))
 }))
 
 export default useSuffleStore
